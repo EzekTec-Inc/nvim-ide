460274395580177f4c6b92b2790ee8fbaec4f251
engr. Stephen Ezekwem <stephen.ezekwem@ezektec.com>
2026-02-26 13:16:29 -0700

    fix(lsp): replace deprecated sign_define with vim.diagnostic.config for Neovim 0.11+

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -21,31 +21,50 @@ return {
     }
 
     -- error lens
-    vim.fn.sign_define {
-      {
-        name = 'DiagnosticSignError',
-        text = '',
-        texthl = 'DiagnosticSignError',
-        linehl = 'ErrorLine',
-      },
-      {
-        name = 'DiagnosticSignWarn',
-        text = '',
-        texthl = 'DiagnosticSignWarn',
-        linehl = 'WarningLine',
-      },
-      {
-        name = 'DiagnosticSignInfo',
-        text = '',
-        texthl = 'DiagnosticSignInfo',
-        linehl = 'InfoLine',
-      },
-      {
-        name = 'DiagnosticSignHint',
-        text = '',
-        texthl = 'DiagnosticSignHint',
-        linehl = 'HintLine',
-      },
-    }
+    if vim.fn.has("nvim-0.11") == 1 then
+      vim.diagnostic.config({
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
+          },
+          linehl = {
+            [vim.diagnostic.severity.ERROR] = 'ErrorLine',
+            [vim.diagnostic.severity.WARN] = 'WarningLine',
+            [vim.diagnostic.severity.INFO] = 'InfoLine',
+            [vim.diagnostic.severity.HINT] = 'HintLine',
+          },
+        },
+      })
+    else
+      vim.fn.sign_define {
+        {
+          name = 'DiagnosticSignError',
+          text = '',
+          texthl = 'DiagnosticSignError',
+          linehl = 'ErrorLine',
+        },
+        {
+          name = 'DiagnosticSignWarn',
+          text = '',
+          texthl = 'DiagnosticSignWarn',
+          linehl = 'WarningLine',
+        },
+        {
+          name = 'DiagnosticSignInfo',
+          text = '',
+          texthl = 'DiagnosticSignInfo',
+          linehl = 'InfoLine',
+        },
+        {
+          name = 'DiagnosticSignHint',
+          text = '',
+          texthl = 'DiagnosticSignHint',
+          linehl = 'HintLine',
+        },
+      }
+    end
   end,
 }

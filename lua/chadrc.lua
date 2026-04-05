b2d8775f4d636bce9fb9f393ab9adf61bea30e9c
EzekTec <stephen.ezekwem@ezektec.com>
2026-04-05 16:40:50 -0600

    chore: update plugins and switch theme to solarized_osaka

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -8,7 +8,7 @@
 --
 -- -- M.ui = {theme = 'catppuccin'} -- This was the default I selected initially
 -- M.ui = {
--- 	theme = "gruvchad",
+-- 	theme = "solarized_osaka",
 --
 -- 	-- hl_override = {
 -- 	-- 	Comment = { italic = true },
@@ -99,18 +99,18 @@ end
 local options = {
 
   base46 = {
-    theme = "gruvchad", -- default theme
+    theme = "solarized_osaka", -- default theme
     hl_add = {},
     hl_override = {},
     integrations = {},
     changed_themes = {},
     transparency = true,
-    theme_toggle = { "gruvchad", "catppuccin_light" },
+    theme_toggle = { "solarized_osaka", "catppuccin_light" },
   },
 
   ui = {
-    theme = "gruvchad",
-    theme_toggle = { "gruvchad", "catppuccin_light" },
+    theme = "solarized_osaka",
+    theme_toggle = { "solarized_osaka", "catppuccin_light" },
 
     cmp = {
       icons = true,
